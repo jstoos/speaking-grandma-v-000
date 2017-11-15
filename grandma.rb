3c9b8d7d56c4a01250a68635d8f0014351285979
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-15 02:39:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I lOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
   end
 end
 
